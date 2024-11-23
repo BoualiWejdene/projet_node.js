202a278e032ddf1643b2354fa515eb04fc15c288
BoualiWejdene <146763358+BoualiWejdene@users.noreply.github.com>
2024-11-23 02:11:50 +0100

    .

--- projet_nodejs/server.js
+++ projet_nodejs/server.js
@@ -3,6 +3,16 @@ const express=require("express")
 const app = express();
 const session = require("express-session");
 app.set("view engine","ejs");
+const http = require("http"); // Importer le module HTTP
+
+
+const socketIo = require('socket.io');
+// Créer un serveur HTTP
+const server = http.createServer(app);
+
+// Créer une instance de socket.io et la lier au serveur HTTP
+const io = socketIo(server);
+
 //connexion a mongodb
 const url = 'mongodb://127.0.0.1:27017';
 const dbName = 'db_elections';
@@ -21,6 +31,19 @@ MongoClient.connect(url)
     db = client.db(dbName);
 })
 .catch(err => {console.error(error)});
+
+
+// Exemple de route pour servir la page
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html');  // Assurez-vous que le chemin est correct
+//   });
+  
+//   // Exemple d'écoute des connexions Socket.IO
+//   io.on('connection', (socket) => {
+//     console.log('Un utilisateur est connecté');
+  
+//     // Écoutez les événements du côté client ici...
+//   });
 
 // signup
 app.get("/signup",async(req,res)=>{
@@ -131,51 +154,103 @@ app.get('/profile/:id', async (req, res) => {
 });
 
 
-app.post('/vote/:id', async (req, res) => {
-  try {
-      const candidatId = req.params.id;
-      const userId = req.session.userId;
+// app.post('/vote/:id', async (req, res) => {
+//   try {
+//       const candidatId = req.params.id;
+//       const userId = req.session.userId;
 
-      if (!ObjectId.isValid(candidatId)) {
-          return res.status(400).send("ID du candidat invalide");
-      }
+//       if (!ObjectId.isValid(candidatId)) {
+//           return res.status(400).send("ID du candidat invalide");
+//       }
 
-      const candidat = await db.collection("Candidat").findOne({ _id: new ObjectId(candidatId) });
-      if (!candidat) {
-          return res.status(404).send("Candidat non trouvé");
-      }
+//       const candidat = await db.collection("Candidat").findOne({ _id: new ObjectId(candidatId) });
+//       if (!candidat) {
+//           return res.status(404).send("Candidat non trouvé");
+//       }
 
-      const user = await db.collection("Utilisateurs").findOne({ _id: new ObjectId(userId) });
-      if (!user) {
-          return res.status(404).send("Utilisateur non trouvé");
-      }
+//       const user = await db.collection("Utilisateurs").findOne({ _id: new ObjectId(userId) });
+//       if (!user) {
+//           return res.status(404).send("Utilisateur non trouvé");
+//       }
 
   
-      const voteExist = await db.collection("Votes").findOne({ userId: new ObjectId(userId), candidatId: new ObjectId(candidatId) });
+//       const voteExist = await db.collection("Votes").findOne({ userId: new ObjectId(userId), candidatId: new ObjectId(candidatId) });
 
-      if (voteExist) {
-          return res.render('view_candidat_profile', { 
-              candidat, 
-              user, 
-              hasVoted: true, 
-              alertMessage: "Vous avez déjà voté pour ce candidat." // Message d'alerte
-          });
-      }
+//       if (voteExist) {
+//           return res.render('view_candidat_profile', { 
+//               candidat, 
+//               user, 
+//               hasVoted: true, 
+//               alertMessage: "Vous avez déjà voté pour ce candidat." // Message d'alerte
+//           });
+//       }
 
-      await db.collection("Votes").insertOne({ userId: new ObjectId(userId), candidatId: new ObjectId(candidatId) });
+//       await db.collection("Votes").insertOne({ userId: new ObjectId(userId), candidatId: new ObjectId(candidatId) });
 
       
-      await db.collection("Candidat").updateOne(
-          { _id: new ObjectId(candidatId) },
-          { $inc: { nbVotes: 1 } }
-      );
+//       await db.collection("Candidat").updateOne(
+//           { _id: new ObjectId(candidatId) },
+//           { $inc: { nbVotes: 1 } }
+//       );
 
+//       res.redirect(`/candidat/${candidatId}`);
+//   } catch (err) {
+//       console.error("Erreur lors du vote :", err);
+//       res.status(500).send("Erreur serveur.");
+//   }
+// });
+
+app.post('/vote/:id', async (req, res) => {
+    try {
+      const candidatId = req.params.id;
+      const userId = req.session.userId;
+  
+      if (!ObjectId.isValid(candidatId)) {
+        return res.status(400).send("ID du candidat invalide");
+      }
+  
+      const candidat = await db.collection("Candidat").findOne({ _id: new ObjectId(candidatId) });
+      if (!candidat) {
+        return res.status(404).send("Candidat non trouvé");
+      }
+  
+      const user = await db.collection("Utilisateurs").findOne({ _id: new ObjectId(userId) });
+      if (!user) {
+        return res.status(404).send("Utilisateur non trouvé");
+      }
+  
+      // Vérifier si l'utilisateur a déjà voté
+      const voteExist = await db.collection("Votes").findOne({ userId: new ObjectId(userId), candidatId: new ObjectId(candidatId) });
+  
+      if (voteExist) {
+        return res.render('view_candidat_profile', { 
+          candidat, 
+          user, 
+          hasVoted: true, 
+          alertMessage: "Vous avez déjà voté pour ce candidat." 
+        });
+      }
+  
+      // Enregistrer le vote
+      await db.collection("Votes").insertOne({ userId: new ObjectId(userId), candidatId: new ObjectId(candidatId) });
+  
+      // Mettre à jour le nombre de votes du candidat
+      await db.collection("Candidat").updateOne(
+        { _id: new ObjectId(candidatId) },
+        { $inc: { nbVotes: 1 } }
+      );
+  
+      // Émettre les résultats en temps réel
+      const candidats = await db.collection("Candidat").find().toArray();
+      io.emit('resultats', candidats);  // Envoie des résultats à tous les clients
+  
       res.redirect(`/candidat/${candidatId}`);
-  } catch (err) {
+    } catch (err) {
       console.error("Erreur lors du vote :", err);
       res.status(500).send("Erreur serveur.");
-  }
-});
+    }
+  });
+  
 
 // Ajouter candidat à la liste des favoris 
 app.post('/favori/:id', async (req, res) => {
@@ -293,6 +368,42 @@ app.post('/profile/update/:id', async (req, res) => {
         res.status(500).send("Erreur serveur.");
     }
 });
+
+// Résultat
+// Route pour afficher les résultats
+// app.get('/resultats', async (req, res) => {
+//     try {
+//       const candidats = await db.collection('Candidat').find().toArray();
+//       res.render('view_resultats', { candidats });
+//     } catch (err) {
+//       console.error('Erreur lors de la récupération des résultats:', err);
+//       res.status(500).send('Erreur serveur.');
+//     }
+//   });
+  
+  // Fonction pour émettre les résultats en temps réel
+//   function emitResults() {
+//     db.collection('Candidat').find().toArray()
+//       .then(candidats => {
+//         io.emit('resultats_update', { candidats }); // Envoi des résultats à tous les clients
+//       })
+//       .catch(err => console.error(err));
+//     }
+    
+    // Route pour simuler la mise à jour des résultats (par exemple, lors d'un vote)
+// app.post('/updateResults', (req, res) => {
+//     // Mettez à jour les résultats dans la base de données (exemple)
+//     db.collection('Candidat').updateOne(
+//       { _id: req.body.candidatId },
+//       { $inc: { nbVotes: 1 } } // Incrémente le nombre de votes
+//     ).then(() => {
+//       emitResults(); // Emet les résultats en temps réel
+//       res.send('Résultats mis à jour');
+//     }).catch(err => {
+//       console.error('Erreur de mise à jour des résultats:', err);
+//       res.status(500).send('Erreur serveur.');
+//     });
+//   });
 
 app.listen(4000, () => {
   console.log("Server running on port 4000");
